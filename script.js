ad40eaf5af666b4e15e730e386725a47b81e1128
Velkur Dharani <dharanivelkur@gmail.com>
2023-04-20 07:24:04 +0530

    added search button

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-let word=prompt("enter a word to search(EX:search,pronunciation,hello,word,work....etc.)")
+
+function abc(){
+let word=prompt("enter a word to search(EX:search,pronunciation,hello,word,work....etc.)");
+
 let API=fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
 API.then((response)=>{return response.json()}).
 then((data)=>{
-    console.log(data)
-    // if(data[0].word==word){
+    // console.log(data)
 let datas=[data[0].word,data[0].meanings[0].partOfSpeech,data[0].phonetics[0].audio,
 data[0].meanings[0].definitions[0].definition,data[0].meanings[0].definitions[0].example,
 data[0].meanings[0].definitions[0].synonyms,data[0].meanings[0].definitions[0].antonyms,data[0].sourceUrls[0]];
 call(datas);
-    // }
-    // else{
-    //    let ans=[data.title,data.message,data.resolution];
-    //    console.log(ans)
-    //    call1(ans);
-    // }
+
+}).catch((error)=>{
+  document.body.innerHTML=`<h1>The word You searched is not available!!! Please search another one.</h1>
+                <button class="btn btn-primary" onclick="myfunction()">search for another one</button>`
+
 })
 call=([word,partsofspeech,sound,definition,example,synonyms,antonyms,refurl])=>{
 document.body.innerHTML=`<div class="card">
@@ -27,17 +28,14 @@ document.body.innerHTML=`<div class="card">
 
   <p>Synonyms : ${synonyms}</p>
   <p>Antonyms : ${antonyms}</p>
-  <a href=${refurl} target="_blank" class="btn btn-primary">Visit her to learn More</a>
-
+  <a href=${refurl} target="_blank" class="btn btn-primary">Visit here to learn More</a>
+  <button class="btn btn-primary" onclick="myfunction()">Search another word</button>
 </div>
 </div>`
 }
-// call1=([ttitl,msg,resolution])=>{
-//     document.body.innerHTML=`div class="card">
-//     <div class="card-body">
-//       <h5 class="card-title">${ttitl}</h5>
-//       <p class="card-text">Message: ${msg}</p>
-//       <p class="card-text">Resolution : ${resolution}</p>
-//     </div>
-//     </div>`
-// }
+}
+
+abc();
+function myfunction(){
+  abc();
+}
